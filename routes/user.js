7a5826d648b7b8536b024c6ab531f7bd6637bcc3
pparksso <thgml5143@hanmail.net>
2022-08-28 13:30:12 +0900

    회원가입 구현

--- routes/user.js
+++ routes/user.js
@@ -17,43 +17,75 @@ router.post("/join", (req, res) => {
   const id = req.body.id;
   const pw = req.body.pw;
   const nickname = req.body.nickname;
-  bcrypt.hash(pw, saltRounds, (err, hash) => {
-    db.collection("count").findOne({ name: "user" }, (err, result) => {
-      const userNum = result.count + 1;
-      if (err) {
-        console.log(err, "user count find err");
-        res.send(`<script>alret("회원가입에 실패하였습니다. 다시한번 시도해주세요.")</script>`);
-      }
-      db.collection("user").insertOne(
-        {
-          userNum: userNum,
-          id: id,
-          pw: hash,
-          nickname: nickname,
-        },
-        (err, result) => {
-          if (err) {
-            console.log(err, "user info insert err");
-            res.send(`<script>alret("회원가입에 실패하였습니다. 다시한번 시도해주세요.")</script>`);
-          }
-          db.collection("count").updateOne(
-            { name: "user" },
-            {
-              $inc: {
-                count: 1,
-              },
-            },
-            (err, result) => {
-              if (err) {
-                console.log(err, "user count update err");
-                res.send(`<script>alret("회원가입에 실패하였습니다. 다시한번 시도해주세요.")</script>`);
-              }
-            }
-          );
+  const pwCheck = req.body.pwCheck;
+  if (pw !== pwCheck) {
+    res.json({ pwCheck: false });
+  }
+  if (pw === pwCheck) {
+    bcrypt.hash(pw, saltRounds, (err, hash) => {
+      db.collection("count").findOne({ name: "user" }, (err, result) => {
+        const userNum = result.count + 1;
+        if (err) {
+          console.log(err, "user count find err");
+          res.send(`<script>alret("회원가입에 실패하였습니다. 다시한번 시도해주세요.")</script>`);
         }
-      );
-      res.json({ isjoin: true });
+        db.collection("user").insertOne(
+          {
+            userNum: userNum,
+            id: id,
+            pw: hash,
+            nickname: nickname,
+          },
+          (err, result) => {
+            if (err) {
+              console.log(err, "user info insert err");
+              res.send(`<script>alret("회원가입에 실패하였습니다. 다시한번 시도해주세요.")</script>`);
+            }
+            db.collection("count").updateOne(
+              { name: "user" },
+              {
+                $inc: {
+                  count: 1,
+                },
+              },
+              (err, result) => {
+                if (err) {
+                  console.log(err, "user count update err");
+                  res.send(`<script>alret("회원가입에 실패하였습니다. 다시한번 시도해주세요.")</script>`);
+                }
+              }
+            );
+          }
+        );
+        res.json({ isjoin: true });
+      });
     });
+  }
+});
+router.post("/idcheck", (req, res) => {
+  const id = req.body.id;
+  db.collection("user").findOne({ id: id }, (err, result) => {
+    if (err) {
+      res.send(`<script>alret("id체크에 실패했습니다. 다시 한번 시도해주세요.")</script>`);
+    }
+    if (!result) {
+      res.json({ idCheck: true });
+    } else {
+      res.json({ idChech: false });
+    }
+  });
+});
+router.post("/nicknamecheck", (req, res) => {
+  const nickname = req.body.nickname;
+  db.collection("user").findOne({ nickname: nickname }, (err, result) => {
+    if (err) {
+      res.send(`<script>alret("닉네임체크에 실패했습니다. 다시 한번 시도해주세요.")</script>`);
+    }
+    if (!result) {
+      res.json({ isNicknameCheck: true });
+    } else {
+      res.json({ isNicknameCheck: false });
+    }
   });
 });
 router.get("/mypage", (req, res) => {
