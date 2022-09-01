0bf179d3ae8f78f56b5074b88bac9e528d039eea
pparksso <thgml5143@hanmail.net>
2022-09-02 07:52:51 +0900

    리드미 진행사항

---
+++ routes/heart.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+const MongoClient = require("mongodb").MongoClient;
+let db = null;
+MongoClient.connect(process.env.MONGO_URL, { useUnifiedTopology: true }, (err, client) => {
+  if (err) {
+    console.log(err, "db connecting err");
+  }
+  console.log("====db connect");
+  db = client.db("travelApp");
+});
